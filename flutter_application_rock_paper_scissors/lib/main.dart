d31385aeeed7b483c744d996f453e750a4088a73
hesahaljenfawi <hesahaljenfawi@gmail.com>
2022-09-14 20:32:49 +0300

    dayFive

---
+++ flutter_application_rock_paper_scissors/lib/main.dart
@@ -0,0 +1,72 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var num1 = 1;
+
+  var num2 = 1;
+
+  void play() {
+    num1 = Random().nextInt(3) + 1;
+    num2 = Random().nextInt(3) + 1;
+
+    setState(() {});
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lime,
+          title: Text('Rock . Paper . Scissor . GO!'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      padding: EdgeInsets.all(10),
+                      height: 80,
+                      width: 80,
+                      child: Image.asset('assets/i_$num1.png')),
+                  Container(
+                      padding: EdgeInsets.all(10),
+                      height: 80,
+                      width: 80,
+                      child: Image.asset('assets/i_$num2.png')),
+                ],
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.lime,
+                ),
+                onPressed: () {
+                  play();
+                },
+                child: Text('GO!'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
